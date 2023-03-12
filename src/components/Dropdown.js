6d0ec3ee046bbc0a502b57585af1608a8aa9e472
ugopulito <pulito.ugo@gmail.com>
2023-03-12 10:15:07 +0100

    anim chevron dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = (props) => {
-    const log = () => {
-        console.log('clic sur ' + props.title);
+    const [opened, setOpened] = useState(false);
+    const handleClick = () => {
+        setOpened(!opened);
     }
     return (
         <div className='dropdown'>
-            <h3 onClick={log}>{props.title}</h3>
-            <>{props.text}</>
+            <h3 onClick={handleClick}>{props.title} {opened ? <img className='chevron up' src='/assets/img/chevron.png' alt='chevron'/> : <img className='chevron down' src='/assets/img/chevron.png' alt='chevron'/>}</h3>
+            {opened ? props.text : <p className='closed'></p>}
         </div>
     );
 };
